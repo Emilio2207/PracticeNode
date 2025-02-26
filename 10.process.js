c95fdc547916cdf7fb74e8a3896c7b9f23dea27d
EmilioRamirez <emi.emilioramirez@gmail.com>
2025-02-26 16:42:17 -0300

    CWD LS commit

---
+++ 10.process.js
@@ -0,0 +1,16 @@
+// argumentos de entrada
+// console.log(process.argv);
+
+// controlar el proceso y su salida
+// process.exit(); // con 0 y 1
+
+// podemos controlar eventos del proceso
+// process.on('exit', () => {
+// limpiar los recursos
+// });
+
+// current working directory
+console.log(process.cwd());
+
+// platform
+console.log(process.env.PEPITO);
