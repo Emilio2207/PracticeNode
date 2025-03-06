37b13def886dd5ef8db086eb76f13449e634159b
EmilioRamirez <emi.emilioramirez@gmail.com>
2025-03-06 15:48:27 -0300

    free port commit

--- 12.http.js
+++ 12.http.js
@@ -1,10 +1,15 @@
 const http = require('node:http');
+const { findAvailablePort } = require('./13.free-port.js');
+
+const desiredPort = process.env.PORT ?? 3000;
 
 const server = http.createServer((req, res) => {
   console.log('resquest received');
   res.end('Hola mundo');
 });
 
-server.listen(0, () => {
-  console.log(`server listening on port http://localhost:${server.address().port}`);
+findAvailablePort(desiredPort).then((port) => {
+  server.listen(port, () => {
+    console.log(`server listening on port http://localhost:${port}`);
+  });
 });
